9bdbacf253fb3ee9642035dd60df23d9e589195d
Edwin Alexander <eabohorquezg@unal.edu.co>
2015-09-18 19:29:15 -0500

    Adiciono el main del proyecto

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        User person;
+        person = new User("Edwin", "Alexander", 22, "eabohorquezg", "mypassword");
+        System.out.print("Usuario de Foro : ");
+        System.out.println(person.getName());
+    }
+    
+}
